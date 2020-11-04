d5877aa35c408af3c3aad2903a1a05b5e36d5415
Stéphan Carion <stephan.carion@gmail.com>
2020-11-04 11:21:52 +0100

    classe StageModel : paramètres + constructeur

--- GestionStage/src/be/technifutur/java2020/gestionStage/stage/StageModel.java
+++ GestionStage/src/be/technifutur/java2020/gestionStage/stage/StageModel.java
@@ -1,4 +1,23 @@
 package be.technifutur.java2020.gestionStage.stage;
 
+import java.time.LocalDateTime;
+
 public class StageModel {
+    private String name;
+    private LocalDateTime dateHeureDebut;
+    private LocalDateTime dateHeureFin;
+
+    public StageModel(String name, LocalDateTime dateHeureDebut, LocalDateTime dateHeureFin){
+        if(name.length() > 0){
+            this.name=name;
+            if (dateHeureFin.compareTo(dateHeureDebut) > 0){
+                this.dateHeureDebut=dateHeureDebut;
+                this.dateHeureFin=dateHeureFin;
+            }else{
+                throw invalidEndDateTimeStageException;
+            }
+        }else{
+            throw emptyNameStageException;
+        }
+    }
 }
