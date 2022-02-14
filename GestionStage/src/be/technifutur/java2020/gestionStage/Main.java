23beee5f0c1480cb8a5d95ea88888cddadca2060
Stéphan <stephan.carion@gmail.com>
2022-02-14 13:55:02 +0100

    Optimisation import

--- GestionStage/src/be/technifutur/java2020/gestionStage/Main.java
+++ GestionStage/src/be/technifutur/java2020/gestionStage/Main.java
@@ -1,13 +1,5 @@
 package be.technifutur.java2020.gestionStage;
 
-import be.technifutur.java2020.gestionStage.exception.ChaineDeCaractereVideException;
-import be.technifutur.java2020.gestionStage.exception.DureeNegativeOuEgaleAZeroException;
-
-import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.TreeSet;
-
 public class Main {
     public static void main(String[] args) {
         Factory factory = new Factory();
